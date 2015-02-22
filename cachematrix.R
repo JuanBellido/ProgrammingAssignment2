d9f7b43fd412ae08463d1f7b22bf4a2e9fdbb57e
JuanBellido <juan@bellido.com>
2015-02-22 23:12:42 +0100

    Programming Assignment 2 from Juan Bellido for Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,14 @@
-#This script contains two functions: makeCacheMatrix and cacheSolve
+#This script contains two functions: makeCacheMatrix and cacheSolve. The idea is
 #
 #makeCacheMatrix is an object-function- that has a matrix as argument.The object 
 #       contains four functions/methods: set,get,setInverse and getInverse 
 #       that are accessed from a list in which each element is named as 
 #       the function it calls. 
 #       This function stores the matrix passed through the initialization 
-#       argument in the variable mtxOrig, and returns a list of functions:
-#       -The 'set' function sets its argument into the variable mtxOrig,
+#       argument in the variable mtxInput, and returns a list of functions:
+#       -The 'set' function sets its argument into the variable mtxInput,
 #        and sets mtxInv-stored in cacheSolve- to NULL
-#       -The 'get'function returns mtxOrig.
+#       -The 'get'function returns mtxInput.
 #       -The 'getInverse' returns mtxInv, a variable stored in cacheSolve
 #       -The 'setInverse' stores its argument in mtxInv-cacheSolve-.
 #
@@ -20,19 +20,21 @@
 
 
 #Stores an input matrix (argument) and returns a list containing a function to
-#get (returns input matrix) ,set (set a new input matrix),
-#getInverse (returns input matrix inverse if cacheSolve executed in object) 
-#and setInverse (sets the input matrix inverse in cacheSolve mtxInv)
-makeCacheMatrix <- function(mtxOrig = matrix()) {
+#'get' returns input matrix-mtxInput
+#'set' resets the input matrix-mtxInput, and reset mtxInv to NULL
+#'getInverse' returns mtxInv from cacheSolve (inverse of mtxInput if
+# cacheSolve already executed against current object-mtxInput- else NULL) 
+#'setInverse' sets the argument into cacheSolve mtxInv
+makeCacheMatrix <- function(mtxInput = matrix()) {
         
         mtxInv <- NULL
         
         set <- function(y) {
-                mtxOrig <<- y
+                mtxInput <<- y
                 mtxInv <<- NULL
         }
         
-        get <- function() mtxOrig
+        get <- function() mtxInput
         
         setInverse <- function(inv) mtxInv <<- inv
         
@@ -49,7 +51,7 @@ makeCacheMatrix <- function(mtxOrig = matrix()) {
 #       objective is to return and store in mtxInv the inverse of the matrix 
 #       contained in the makeCacheMatrix used as input.It will only calculate
 #       the inverse and store it in mtxInv when it has not been calculated
-#       before (if calculated before it is already stored in mtxInv,and
+#       before (if calculated before, it is already stored in mtxInv,and
 #       the function gives a message that is getting cached data).
 cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
